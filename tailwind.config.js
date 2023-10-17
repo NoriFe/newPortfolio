9baaccd800237e06aab48e186f3d5dacf1326728
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2023-10-17 07:45:58 +0100

    Coloros and font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg:'#020916',
+        accent:'#70FF00',
+      },
+      fontFamily: {
+        dnsans: 'DM Sans, sans-serif'
+      }
+    },
   },
   plugins: [],
 }
