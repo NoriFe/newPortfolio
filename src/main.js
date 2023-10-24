2e56896088cbbb6f936734159b5e80deba8d7b91
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2023-10-24 11:47:25 +0100

    fixing tag error

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ fetch(`https://api.github.com/users/${username}/repos`)
 const {description, homepage, html_url, name, stargazers_count, topics} = repository;
 
 let tags =``;
-for (tag of topics) {
+for (let tag of topics) {
     tags += `<li class="bg-gray-400/10 py-1 px-2 rounded text-sm font-bold">${tag}</li>`
    
 }
