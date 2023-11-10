af84849c8a03bccc4283694241ec94113d3f568c
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2023-11-10 13:28:54 +0000

    cors

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://norbert-knows.co.uk');
- 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
+});
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 
