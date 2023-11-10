e4772832d196bbc8aecda2445d2febfb5ef9536e
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2023-11-10 13:36:23 +0000

    another cros update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://norbert-knows.co.uk');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://norbert-knows.co.uk');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 
 
